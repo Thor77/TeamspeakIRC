cf4e89b595bcfb46a42577636bf1bf868fced6e4
Thor77 <xXThor77Xx@gmail.com>
2015-08-04 19:39:10 +0200

    give BeautifulSoup parser-option

--- nplstatus.py
+++ nplstatus.py
@@ -5,6 +5,6 @@ import cfscrape
 def get():
     scraper = cfscrape.create_scraper()
     data = scraper.get('http://npl.teamspeakusa.com/ts3npl.php').content
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data, 'html.parser')
     raw_status = soup.find_all(class_='register_linklabel')[2].span
     return not raw_status
